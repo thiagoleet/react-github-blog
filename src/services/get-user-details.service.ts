12208c641a084a85590da2d68bcdbe0655d8acbe
Thiago Leite <thiagofmleite@gmail.com>
2025-01-31 09:39:35 -0300

    Update getUserDetails service to use User interface for type safety

--- src/services/get-user-details.service.ts
+++ src/services/get-user-details.service.ts
@@ -1,6 +1,7 @@
 import { api } from "../lib/axios";
+import { User } from "../models/User";
 
 export const getUserDetails = async (username: string) => {
-  const response = await api.get(`users/${username}`);
+  const response = await api.get<User>(`users/${username}`);
   return response.data;
 };
